0e13b24c936020f2e4c9f718f7a7f2e5755655ad
Jakub Martin <jakub.wit.martin@gmail.com>
2015-12-21 20:20:25 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World!!!"))
+	})
+	http.ListenAndServe(":3000", nil)
+}
